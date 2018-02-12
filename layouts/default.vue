f0506b897d0d91c9b269b63199daaa5f442b855c
Muhammad Althaf <althf.m@gmail.com>
2018-02-12 22:06:27 +0530

    added cart page

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,53 +1,34 @@
 <template>
   <div>
+    <nav class="navbar is-white has-shadow">
+      <div class="container">
+        <div class="navbar-brand">
+          <router-link to="/" class="navbar-item is-size-3 logo has-text-dark">
+            The<b>Case</b> Company
+          </router-link>
+        </div>
+        <div class="navbar-menu">
+          <div class="navbar-end">
+            <div class="navbar-item">
+              <nuxt-link :to="{name: 'cart'}"> Cart (0)</nuxt-link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </nav>
     <nuxt/>
+    <footer class="footer">
+      <div class="container">
+        <div class="content has-text-centered">
+          Proudly Powered By <a href="https://cosmicjs.com" class="has-text-dark"><strong>Cosmic JS</strong></a>
+        </div>
+      </div>
+    </footer>
   </div>
 </template>
 
 <style>
-html
-{
-  font-family: "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
-  font-size: 16px;
-  word-spacing: 1px;
-  -ms-text-size-adjust: 100%;
-  -webkit-text-size-adjust: 100%;
-  -moz-osx-font-smoothing: grayscale;
-  -webkit-font-smoothing: antialiased;
-  box-sizing: border-box;
-}
-*, *:before, *:after
-{
-  box-sizing: border-box;
-  margin: 0;
-}
-.button--green
-{
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #3b8070;
-  color: #3b8070;
-  text-decoration: none;
-  padding: 10px 30px;
-}
-.button--green:hover
-{
-  color: #fff;
-  background-color: #3b8070;
-}
-.button--grey
-{
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #35495e;
-  color: #35495e;
-  text-decoration: none;
-  padding: 10px 30px;
-  margin-left: 15px;
-}
-.button--grey:hover
-{
-  color: #fff;
-  background-color: #35495e;
+body {
+  background: #DAE2DB;
 }
 </style>
--- nuxt.config.js
+++ nuxt.config.js
@@ -15,7 +15,8 @@ module.exports = {
       { hid: 'description', name: 'description', content: pkg.description }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Nunito:300,400' }
     ]
   },
 
