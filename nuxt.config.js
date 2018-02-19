77f3e35b33ce7e6595369e3f1acfeb82254ecdc5
Tony Spiro <tspiro@tonyspiro.com>
2018-02-19 13:06:03 -0600

    move deps, build on start, default Bucket slug

--- nuxt.config.js
+++ nuxt.config.js
@@ -85,7 +85,7 @@ module.exports = {
   ** Environment variables
   */
   env: {
-    COSMIC_BUCKET: process.env.COSMIC_BUCKET,
+    COSMIC_BUCKET: process.env.COSMIC_BUCKET || 'vue-ecommerce',
     COSMIC_READ_KEY: process.env.COSMIC_READ_KEY,
     STRIPE_PUBLIC_KEY: process.env.STRIPE_PUBLIC_KEY
   }
